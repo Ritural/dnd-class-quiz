5a04553714ed694f7bca3a0a44253f9216917960
Kallan <kallan@alphero.com>
2019-01-29 22:11:21 +1300

    Update main-reducer. Add commented out main-saga.

--- src/state/main-reducer.ts
+++ src/state/main-reducer.ts
@@ -1,16 +1,53 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import { exampleReducer, IExampleState } from 'state/modules/example/reducers';
+import createSagaMiddleware from '@redux-saga/core';
+import { createResponsiveStateReducer, responsiveStoreEnhancer } from 'redux-responsive';
+import { createBrowserHistory } from "history";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+
+import { BREAKPOINTS } from 'state/modules/app/constants';
+// import { rootSaga } from "state/modules/main-saga";
 
 export interface IReducerState {
-  exampleState?: IExampleState;
+  exampleState: IExampleState;
+} 
+
+export const history = createBrowserHistory();
+
+const sagaMiddleware = createSagaMiddleware({});
+const middleware = [ routerMiddleware(history), sagaMiddleware ];
+
+let composeEnhancers = compose;
+
+if (process.env.REACT_APP_ENVIRONMENT !== "production") {
+  const composeWithDevToolsExtension = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+  if (typeof composeWithDevToolsExtension === "function") {
+    composeEnhancers = composeWithDevToolsExtension;
+  }
 }
 
 const rootReducer = combineReducers({
+  browser: createResponsiveStateReducer(
+    {
+      mobile: BREAKPOINTS.MOBILE,
+      tablet: BREAKPOINTS.TABLET,
+      laptop: BREAKPOINTS.LAPTOP,
+      desktop: BREAKPOINTS.DESKTOP
+    },
+    {
+      // This assigns anything bigger than desktop as widescreen
+      infinity: "widescreen"
+    }
+  ),
   exampleState: exampleReducer,
+  router: connectRouter(history)
 });
 
-// export const REDUX_INITIAL_STATE: IReducerState = {
-//   exampleState: EXAMPLE_INITIAL_STATE,
-// };
+export const store = createStore(
+  rootReducer,
+  {},
+  composeEnhancers(responsiveStoreEnhancer, applyMiddleware(...middleware))
+);
 
-export const store = createStore(rootReducer, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__());
+// sagaMiddleware.run(rootSaga);
---
+++ src/state/modules/main-saga.ts
@@ -0,0 +1,8 @@
+// import { fork, all } from 'redux-saga/effects';
+// import { appSaga } from 'state/modules/app/sagas';
+
+// export function* rootSaga() {
+//   yield all([
+//     fork(appSaga),
+//   ]);
+// }
