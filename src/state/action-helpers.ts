759aff6b992c3dd2f0d9baa27f6123d27cf7ca66
Kallan <kallan@alphero.com>
2019-01-29 20:46:18 +1300

    Add potential improvements to action-helpers.

--- src/state/action-helpers.ts
+++ src/state/action-helpers.ts
@@ -12,6 +12,37 @@ export function createAction<T extends string, P>(type: T, payload?: P) {
   return (payload === undefined) ? { type } : { type, payload };
 }
 
+// export const action = (name: string) => (actionName: string) => {
+//   return `${name}-${actionName}`;
+// };
+
+// export type ActionType = {
+//   [key: string]: [ string, boolean? ];
+// }
+
+// type Action = {
+//   [key: string]: string;
+// }
+
+// export function createActions(name: string, actionTypes: ActionType) {
+//   const keys = Object.keys(actionTypes);
+//   const actions: Action = {};
+
+//   keys.forEach((key) => {
+//     const action = actionTypes[key];
+//     const actionName = action[0];
+//     // const isAsync = action[1];
+
+//     // if (isAsync) {
+//     //   actions[`${actionName}_SUCCESS`],
+//     // }
+
+//     actions[actionName] = `[${name}]-${actionName}`;
+//   });
+
+//   return actions;
+// }
+
 // type FunctionType = ( ...args: any[] ) => any;
 // interface IActionCreatorsMapObject {
 //   [actionCreator: string]: FunctionType;
