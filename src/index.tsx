7ed97fc36f583a3173ecdbb280ec656b299d03b8
Kallan <kallan@alphero.com>
2019-01-29 20:17:54 +1300

    Rework the index.tsx structure.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,59 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import posed, { PoseGroup } from "react-pose";
 
 import 'styles/app.scss';
 
+import store from 'state/main-reducer';
+
 import Home from 'routes/home';
-import reducers from 'state/main-reducer';
+import NotFound from "components/not-found";
+import ReduxExample from "components/redux-example";
+
+const RouteContainer = posed.div({
+  enter: {
+    opacity: 1,
+    transition: {
+      default: { ease: "linear", duration: 350 }
+    }
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      default: { ease: "linear", duration: 350 }
+    }
+  }
+});
 
 class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducers, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__())}>
+      <Provider store={store}>
         <BrowserRouter>
-          <Route path='/' component={Home} />
+          <Route render={({ location }) => (
+            <PoseGroup>
+              <RouteContainer key={location.pathname}>
+                <header className='Header'>
+                  <Link className='' to='/'>Home</Link>
+                  <Link className='u-pL-lg' to='/redux-example'>Redux Example</Link>
+                </header>
+
+                <main className='Content u-m-lg'>
+                  <Switch>
+                    <Route exact={true} path='/' component={Home} />
+                    <Route exact={true} path='/redux-example' component={ReduxExample} />
+                    <Route component={NotFound} />
+                  </Switch>
+                </main>
+
+                <footer className='Footer'>
+                  <p>This is the footer</p>
+                </footer>
+              </RouteContainer>
+            </PoseGroup>
+          )} />
         </BrowserRouter>
       </Provider>
     );
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,28 +1,17 @@
 import * as React from 'react';
-import { Switch } from 'react-router';
-import { Route, Link } from 'react-router-dom';
 
-import StartHere from 'components/start-here';
-import NotFound from 'components/not-found';
-import ReduxExample from 'components/redux-example';
+// tslint:disable-next-line:no-empty-interface
+interface IProps {
+}
+// tslint:disable-next-line:no-empty-interface
+interface IState {
+}
 
-export default class Home extends React.Component {
+export default class StartHere extends React.Component<IProps, IState> {
   render() {
     return (
-      <div className='Home u-m-lg'>
-        
-        <header>
-          <Link className='' to='/'>Home</Link>
-          <Link className='u-pL-lg' to='/redux-example'>Redux Example</Link>
-        </header>
-
-        <main className='Content u-m-lg'>
-          <Switch>
-            <Route exact={true} path='/' component={StartHere} />
-            <Route exact={true} path='/redux-example' component={ReduxExample} />
-            <Route component={NotFound} />
-          </Switch>
-        </main>
+      <div className='Home'>
+        <h3>You can start here</h3>
       </div>
     );
   }
