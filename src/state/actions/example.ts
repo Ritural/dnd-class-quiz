f12dcfc69b4efe114fe190a99fd6f4e12406a0e2
Kallan <kallan@alphero.com>
2019-01-29 20:46:33 +1300

    Update actions/example types.

--- src/state/actions/example.ts
+++ src/state/actions/example.ts
@@ -1,8 +1,12 @@
-import { createAction } from 'state/action-helpers';
+import { createAction } from "state/action-helpers";
 
 export const ExampleActionTypes = {
-  INCREMENT_COUNT: '[example]INCREMENT',
-  SET_COUNT: '[example]SET',
+  INCREMENT_COUNT: `[example]-INCREMENT`,
+  SET_COUNT: `[example]-SET`
+
+  // @TODO Async example
+  // SET_COUNT_SUCCESS: '[example]-SET-SUCCESS',
+  // SET_COUNT_ERROR: '[example]-SET-ERROR',
 };
 
 export const actionIncrementCount = () => createAction(ExampleActionTypes.INCREMENT_COUNT);
