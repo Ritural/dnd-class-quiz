29f41d034fdc14659ad2ea24be4e30376dc690c5
Kallan <kallan@alphero.com>
2019-01-31 22:29:11 +1300

    Update webpack common config and add postcss loader.

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.tsx'),
   output: {
@@ -20,10 +22,10 @@ module.exports = {
       utilities: path.resolve(__dirname, 'src/utilities'),
 
       // Has to go at the end, some weird webpack behaviour
-      public: path.resolve(__dirname, 'public/'),
+      public: path.resolve(__dirname, 'public/')
     },
     // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js']
   },
 
   module: {
@@ -31,47 +33,48 @@ module.exports = {
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["source-map-loader"],
-        enforce: "pre"
+        use: ['source-map-loader'],
+        enforce: 'pre'
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
         use: [
-          true ? 'style-loader' : MiniCssExtractPlugin.loader,
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
           {
-            loader: "css-loader", // translates CSS into CommonJS
+            loader: 'css-loader', // 4. translates CSS into CommonJS
             options: {
               sourceMap: true,
-              importLoaders: true,
+              importLoaders: true
             }
           },
-          // {
-          //   loader: 'postcss-loader', // Adds css prefixes
-          // },
-          // "resolve-url-loader", // Resolves relative paths in url() statements
           {
-            loader: "sass-loader", // compiles Sass to CSS
+            loader: 'postcss-loader', // 3. Adds CSS prefixes
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader', // 1. Compiles Sass to CSS
             options: {
               sourceMap: true
             }
           }
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          // { loader: 'postcss-loader' }, // Adds css prefixes
-          // { loader: "resolve-url-loader" }, // Resolves relative paths in url() statements
         ]
       },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     { loader: 'style-loader' },
+      //     { loader: 'css-loader' },
+      //     { loader: 'postcss-loader' }, // Adds css prefixes
+      //   ]
+      // },
       {
         type: 'javascript/auto',
         test: /\.(svg|json)$/,
@@ -87,19 +90,19 @@ module.exports = {
             loader: 'file-loader',
             options: {
               // This can be changed to include [hash:8], (:8 should restrict it to 8 characters)
-              name: 'public/[name].[ext]',
+              name: 'public/[name].[ext]'
             }
           }
         ]
-      },
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       // HtmlWebpackPlugin generates a dynamic index.html file which inserts all our script tags declared above in entry
       template: path.resolve(__dirname, 'src/index.html'), // You can add [chunkhash] to the index.html file as well
-      filename: 'index.html',       // Output filename
-      inject: 'body',               // Where the scripts will be inserted (concat with what's already in body)
+      filename: 'index.html', // Output filename
+      inject: 'body', // Where the scripts will be inserted (concat with what's already in body)
       files: {
         // Require manifest first as it includes webpack file references
         js: ['dist/index.js']
@@ -109,7 +112,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name].css",
+      filename: '[name].css'
       // chunkFilename: "[id].css"
     })
   ]
