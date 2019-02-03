cfc63de8cd96c2a9da1872ca09a7f8e5a07cdec9
Kallan <kallan@alphero.com>
2019-02-03 17:14:01 +1300

    Create icon common component.

---
+++ src/components/common/icon.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import SVG_ICONS from 'public/svg/all';
+
+const ALL_ICONS = {
+  ...SVG_ICONS
+  // IconMoon icons if they need to be in the project
+};
+
+type IconName = keyof typeof ALL_ICONS;
+
+interface IProps {
+  name: IconName;
+  className?: string;
+}
+
+export class Icon extends React.Component<IProps> {
+  render() {
+    const { name } = this.props;
+    const icon = ALL_ICONS[name];
+
+    if (!icon) {
+      console.warn(`No icon found for \<${name}\>`);
+      return null;
+    }
+
+    const isSVG = name in SVG_ICONS;
+    if (isSVG) {
+      const { className } = this.props;
+      const classes = className ? `SVG ${className}` : 'SVG';
+
+      return (
+        <span className={classes} data-icon={name}>
+          {icon}
+        </span>
+      );
+    }
+
+    return null;
+
+    // It's a icon font
+    // return icon;
+  }
+}
