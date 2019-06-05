8111ad24dcef6951a96efef79340db1a3ea8794c
Kallan Jones <kallan.jones@gmail.com>
2019-06-05 14:16:36 +1200

    Update main index file. Add header component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import posed, { PoseGroup } from 'react-pose';
 import { Provider } from 'react-redux';
-import posed, { PoseGroup } from "react-pose";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import 'styles/app.scss';
-
-import { store } from 'store/main-reducer';
-
+import { Header } from 'components/header';
 import { Home } from 'routes/home';
-import { NotFound } from "routes/not-found";
-import { ReduxExample } from "routes/redux-example";
+import { NotFound } from 'routes/not-found';
+import { ReduxExample } from 'routes/redux-example';
+import { store } from 'store/main-reducer';
+import 'styles/app.scss';
 
 const RouteContainer = posed.div({
   enter: {
     opacity: 1,
     transition: {
-      default: { ease: "linear", duration: 350 }
-    }
+      default: { ease: 'linear', duration: 350 },
+    },
   },
   exit: {
     opacity: 0,
     transition: {
-      default: { ease: "linear", duration: 350 }
-    }
-  }
+      default: { ease: 'linear', duration: 350 },
+    },
+  },
 });
 
 class App extends React.Component {
@@ -32,28 +31,31 @@ class App extends React.Component {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <Route render={({ location }) => (
-            <PoseGroup>
-              <RouteContainer key={location.pathname}>
-                <header className='Header'>
-                  <Link className='' to='/'>Home</Link>
-                  <Link className='u-pL-lg' to='/redux-example'>Redux Example</Link>
-                </header>
+          <Route
+            render={({ location }) => (
+              <PoseGroup>
+                <RouteContainer key={location.pathname}>
+                  <Header />
 
-                <main className='Content u-m-lg'>
-                  <Switch>
-                    <Route exact={true} path='/' component={Home} />
-                    <Route exact={true} path='/redux-example' component={ReduxExample} />
-                    <Route component={NotFound} />
-                  </Switch>
-                </main>
+                  <main className='Content u-m-lg'>
+                    <Switch>
+                      <Route exact={true} path='/' component={Home} />
+                      <Route
+                        exact={true}
+                        path='/redux-example'
+                        component={ReduxExample}
+                      />
+                      <Route component={NotFound} />
+                    </Switch>
+                  </main>
 
-                <footer className='Footer'>
-                  <p>This is the footer</p>
-                </footer>
-              </RouteContainer>
-            </PoseGroup>
-          )} />
+                  <footer className='Footer'>
+                    <p>This is the footer</p>
+                  </footer>
+                </RouteContainer>
+              </PoseGroup>
+            )}
+          />
         </BrowserRouter>
       </Provider>
     );
