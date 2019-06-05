b24b8e3ef4ff0a09111444ffcb76b42a13fbed68
Kallan Jones <kallan.jones@gmail.com>
2019-06-05 14:16:27 +1200

    Tidy up main-reducer.

--- src/store/main-reducer.ts
+++ src/store/main-reducer.ts
@@ -1,28 +1,32 @@
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import { exampleReducer, IExampleState } from 'store/modules/example/reducers';
 import createSagaMiddleware from '@redux-saga/core';
-import { createResponsiveStateReducer, responsiveStoreEnhancer } from 'redux-responsive';
-import { createBrowserHistory } from "history";
-import { connectRouter, routerMiddleware } from "connected-react-router";
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import {
+  createResponsiveStateReducer,
+  responsiveStoreEnhancer,
+} from 'redux-responsive';
 
 import { BREAKPOINTS } from 'store/modules/app/constants';
+import { exampleReducer, IExampleState } from 'store/modules/example/reducers';
 // import { rootSaga } from "store/modules/main-saga";
 
 export interface IReducerState {
   exampleState?: IExampleState;
-} 
+}
 
 export const history = createBrowserHistory();
 
 const sagaMiddleware = createSagaMiddleware({});
-const middleware = [ routerMiddleware(history), sagaMiddleware ];
+const middleware = [routerMiddleware(history), sagaMiddleware];
 
 let composeEnhancers = compose;
 
-if (process.env.REACT_APP_ENVIRONMENT !== "production") {
-  const composeWithDevToolsExtension = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+if (process.env.REACT_APP_ENVIRONMENT !== 'production') {
+  const composeWithDevToolsExtension = (window as any)
+    .__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
-  if (typeof composeWithDevToolsExtension === "function") {
+  if (typeof composeWithDevToolsExtension === 'function') {
     composeEnhancers = composeWithDevToolsExtension;
   }
 }
@@ -33,21 +37,21 @@ const rootReducer = combineReducers({
       mobile: BREAKPOINTS.MOBILE,
       tablet: BREAKPOINTS.TABLET,
       laptop: BREAKPOINTS.LAPTOP,
-      desktop: BREAKPOINTS.DESKTOP
+      desktop: BREAKPOINTS.DESKTOP,
     },
     {
       // This assigns anything bigger than desktop as widescreen
-      infinity: "widescreen"
-    }
+      infinity: 'widescreen',
+    },
   ),
   exampleState: exampleReducer,
-  router: connectRouter(history)
+  router: connectRouter(history),
 });
 
 export const store = createStore(
   rootReducer,
   {},
-  composeEnhancers(responsiveStoreEnhancer, applyMiddleware(...middleware))
+  composeEnhancers(responsiveStoreEnhancer, applyMiddleware(...middleware)),
 );
 
 // sagaMiddleware.run(rootSaga);
