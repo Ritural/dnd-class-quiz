80fd07b59129301078e82ab8ce2cd131a4980397
Kallan Jones <kallan.jones@gmail.com>
2019-06-11 21:55:05 +1200

    Updatte redux-example to include get users behaviour.

--- src/routes/redux-example.tsx
+++ src/routes/redux-example.tsx
@@ -3,20 +3,30 @@ import { connect } from 'react-redux';
 
 import { AppState } from 'store/main-reducer';
 import {
+  actionGetUsers,
   actionIncrementCount,
   actionSetCount,
+  IUser,
 } from 'store/modules/example/actions';
 
 interface IProps {
   count: number;
+  users: IUser[];
   incrementCount: typeof actionIncrementCount;
   setCount: typeof actionSetCount;
+  getUsers: typeof actionGetUsers;
 }
 
 // Export presenter for testing
 export class ReduxExampleComponent extends React.Component<IProps> {
+  componentWillMount() {
+    const { getUsers } = this.props;
+
+    getUsers();
+  }
+
   render() {
-    const { count, incrementCount, setCount } = this.props;
+    const { count, incrementCount, setCount, users } = this.props;
 
     return (
       <div className='ReduxExample'>
@@ -31,6 +41,24 @@ export class ReduxExampleComponent extends React.Component<IProps> {
         >
           Set count to 0
         </button>
+
+        <ul className='ReduxExample-list'>
+          {users.map((user) => {
+            const {
+              id,
+              name,
+              username,
+              phone,
+            } = user;
+
+            return (
+              <li className='ReduxExample-list-item' key={id}>
+                <h3>{name}</h3>
+                <p>{username} - {phone}</p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
@@ -39,12 +67,14 @@ export class ReduxExampleComponent extends React.Component<IProps> {
 function mapStateToProps(state: AppState) {
   return {
     count: state.exampleState.count,
+    users: state.exampleState.users,
   };
 }
 
 const dispatchProps = {
   incrementCount: actionIncrementCount,
   setCount: actionSetCount,
+  getUsers: actionGetUsers,
 };
 
 export const ReduxExample = connect(
