06fa14cbf0167d3a7a364a1342a38fbfa8cdd6d2
Kallan Jones <kallan.jones@gmail.com>
2019-06-14 08:40:42 +1200

    Add current branch to footer component.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import { CURRENT_BRANCH } from 'store/modules/app/constants';
+
 // tslint:disable-next-line:no-empty-interface
 interface IProps {
 }
@@ -12,6 +14,7 @@ export class Footer extends React.Component<IProps, IState> {
     return (
       <footer className='Footer'>
         <p>This is the footer</p>
+        <p>{CURRENT_BRANCH}</p>
       </footer>
     );
   }
