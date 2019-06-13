f9301b75949cd3ecffd640adba0e5725038856c3
Kallan Jones <kallan.jones@gmail.com>
2019-06-14 08:40:25 +1200

    Export build constants from the process field.

--- src/store/modules/app/constants.ts
+++ src/store/modules/app/constants.ts
@@ -1,11 +1,6 @@
-// export const MOCK_ENVIRONMENTS = ["development", "test"];
-// export const {
-//   NODE_ENV,
-//   REACT_APP_ENVIRONMENT,
-//   REACT_APP_FEATURE_BRANCH,
-//   REACT_APP_COMMIT_HASH
-// } = process.env;
-// export const ENVIRONMENT = REACT_APP_ENVIRONMENT || NODE_ENV;
-// export const IS_MOCK_ENVIRONMENT = MOCK_ENVIRONMENTS.indexOf(ENVIRONMENT) !== -1;
-// export const FEATURE_BRANCH = REACT_APP_FEATURE_BRANCH;
-// export const COMMIT_HASH = REACT_APP_COMMIT_HASH;
+export const {
+  VERSION,
+  IS_PRODUCTION,
+  IS_DEVELOPMENT,
+  CURRENT_BRANCH,
+} = process.env;
