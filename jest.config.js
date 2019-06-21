8db3a3fff06d46c92d9ef8937dd34f66f313bc73
Kallan Jones <kallan.jones@gmail.com>
2019-06-22 08:58:33 +1000

    Format jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,19 @@
 module.exports = {
   verbose: true,
-  roots: [
-    "<rootDir>/src"
-  ],
+  roots: ['<rootDir>/src'],
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest'
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   globals: {
     'ts-jest': {
       // ...
       diagnostics: {
+        // Check if this is fixed
         ignoreCodes: [151001]
       }
     }
   }
-}
+};
