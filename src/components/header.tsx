b5ab7f78e1bfd782dd3d5d4a8265e9c918e95318
Kallan Jones <kallan.jones@gmail.com>
2019-06-22 09:32:03 +1000

    Update header to loop through menu links.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
+import { MENU_LINKS } from 'store/modules/app/constants';
+
 export class Header extends React.Component {
   render() {
     return (
       <header className='Header'>
-        <Link className='' to='/'>
-          Home
-        </Link>
-        <Link className='u-pL-lg' to='/redux-example'>
-          Redux Example
-        </Link>
+
+        {MENU_LINKS.map(({ to, label }) => (
+          <Link className='Header-link' to={to} key={label}>
+            {label}
+          </Link>
+        ))}
       </header>
     );
   }
