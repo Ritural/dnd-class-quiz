50af062f63d33251850e6d0f326adf29ca0233a1
Kallan Jones <kallan@alphero.com>
2019-09-07 12:21:18 +1200

    Update Header and Footer to pure functions.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,10 @@
 import * as React from 'react';
 
-import { CURRENT_BRANCH } from 'store/modules/app/constants';
+// import { CURRENT_BRANCH } from 'store/modules/app/constants';
 
-// tslint:disable-next-line:no-empty-interface
-interface IProps {
-}
-// tslint:disable-next-line:no-empty-interface
-interface IState {
-}
-
-export class Footer extends React.Component<IProps, IState> {
-  render() {
-    return (
-      <footer className='Footer'>
-        <p>This is the footer</p>
-        <p>{CURRENT_BRANCH}</p>
-      </footer>
-    );
-  }
-}
+export const Footer = () => (
+  <footer className='Footer'>
+    {/* <p>This is the footer</p>
+        <p>{CURRENT_BRANCH}</p> */}
+  </footer>
+);
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,17 +3,12 @@ import { Link } from 'react-router-dom';
 
 import { MENU_LINKS } from 'store/modules/app/constants';
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <header className='Header'>
-
-        {MENU_LINKS.map(({ to, label }) => (
-          <Link className='Header-link' to={to} key={label}>
-            {label}
-          </Link>
-        ))}
-      </header>
-    );
-  }
-}
+export const Header = () => (
+  <header className='Header'>
+    {MENU_LINKS.map(({ to, label }) => (
+      <Link className='Header-link' to={to} key={label}>
+        {label}
+      </Link>
+    ))}
+  </header>
+);
