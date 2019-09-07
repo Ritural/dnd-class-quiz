32d4e2e10c5cdd4814020b377f80175a34cb9998
Kallan Jones <kallan@alphero.com>
2019-09-07 16:48:47 +1200

    Add clear history button to Home componnet.

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,12 +1,34 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export const Home = () => (
+import { actionClearHistory } from 'store/modules/quiz/actions';
+import { Button } from 'components/common/Button/Button';
+
+interface IProps {
+  clearHistory: typeof actionClearHistory;
+}
+
+export const HomeComponent = ({ clearHistory }: IProps) => (
   <section className='Home'>
     <h1>DnD 5th Edition Class Quiz</h1>
 
     <Link className='Button' to='/quiz'>
       Start quiz
     </Link>
+
+    <Button
+      label='Clear history'
+      action={clearHistory}
+    />
   </section>
 );
+
+const dispatchProps = {
+  clearHistory: actionClearHistory,
+};
+
+export const Home = connect(
+  null,
+  dispatchProps,
+)(HomeComponent);
